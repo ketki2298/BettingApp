e0a26738f25f3036656262ed14ff6aeaaeeb14b8
Ketki Kokate <ketkikokate@Ketkis-MacBook-Pro.local>
2020-11-10 01:07:35 -0800

    dependency issue resolve

--- demo.rb
+++ demo.rb
@@ -1,28 +1,13 @@
-# require 'sinatra'
-# require './bets'
-# require "bundler/setup"
-# require 'sinatra/reloader' if development?
-
 require 'sinatra'
 require './bets'
-
-# configure :development, :test do     
-#   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/bet.db")
-# end
-
-# configure :production do
-#   DataMapper.setup(:default, ENV['DATABASE_URL'])
-# end
 
 configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/bet.db")
 end
 
 configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bet.db')
 end
-
-Bets.auto_migrate! unless DataMapper.repository(:default).adapter.storage_exists?('bets')
 
 
 enable :sessions
