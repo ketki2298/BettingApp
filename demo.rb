7803aae17f9b090407d9ad25530a78c501ebb68a
Ketki Kokate <ketkikokate@Ketkis-MacBook-Pro.local>
2020-11-10 00:14:19 -0800

    my app

---
+++ demo.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require './bets'
+require "bundler/setup"
+require 'sinatra/reloader' if development?
+
+configure :development, :test do     
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/bet.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+Bets.auto_migrate! unless DataMapper.repository(:default).adapter.storage_exists?('bets')
+
+
+enable :sessions
+set :username, "ketki"
+set :password, "password"
+
+get '/' do
+    if session[:login]
+        erb :home
+      else
+        erb :login
+      end
+end
+
