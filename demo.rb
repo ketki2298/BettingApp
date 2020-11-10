e1844407e0167e21a2a55217d938cab3563aa3bc
Ketki Kokate <ketkikokate@Ketkis-MacBook-Pro.local>
2020-11-10 01:02:52 -0800

    dependency issue resolve

--- demo.rb
+++ demo.rb
@@ -19,7 +19,7 @@ configure :development do
 end
 
 configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/bet.db")
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
 Bets.auto_migrate! unless DataMapper.repository(:default).adapter.storage_exists?('bets')
