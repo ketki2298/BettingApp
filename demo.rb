36f07484f60693ca8c9b5b3269cd06f2150a97a3
Ketki Kokate <ketkikokate@Ketkis-MacBook-Pro.local>
2020-11-09 23:49:20 -0800

    Added db configs

--- demo.rb
+++ demo.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
 require './bets'
 require "bundler/setup"
+require 'sinatra/reloader' if development?
 
 configure :development, :test do     
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/bet.db")
 end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bet.db')
+end
+
+Bets.auto_migrate! unless DataMapper.repository(:default).adapter.storage_exists?('bets')
+
 
 enable :sessions
 set :username, "ketki"
